3b200ce38cfaf2201a759968e1c1d5368095b715
5erg1kal <sergio.santamaria.next@bbva.com>
2019-11-18 21:24:50 +0100

    refactor server

--- main.js
+++ main.js
@@ -7,29 +7,41 @@ let apiRoutes = require("./routes/test.route.js")
 var sessionController = require('./controllers/sessionController');
 var fs = require('fs');
 var https = require('https');
+var socketIO;
 
 var options = {
     key: fs.readFileSync('./sec/file.pem'),
-    cert: fs.readFileSync('./sec/file.crt')
+    cert: fs.readFileSync('./sec/file.crt'),
+    passphrase: process.env.HTTPS_PASSPHRASE || ''
   };
-var securityServerPort = 443;
 
-var server = require('http').Server(app);
+var securityServerPort = process.env.PORT || 3031;
+
+var port = process.env.PORT || 8080;
+
 var securityServer = https.createServer(options, app);
-
-var socketPort = process.env.PORT || 3031;
-var socketIO;
 
 var secIO = require('socket.io')(securityServer);
 
-//var io = require('socket.io')(server, { origins: '*:*', forceNew: true });
-
-/*server.listen(socketPort, function() {
-	console.log('socket server running on 3031 port');
-});*/
-
 securityServer.listen(securityServerPort, function() {
 	console.log('socket server running on '+securityServerPort+' port');
+});
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "'GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    bodyParser.urlencoded({ extended: true });
+    bodyParser.json();
+    next();
+});
+
+app.use(express.static(process.env.SERVE_DIRECTORY || 'web'));
+
+app.use('/', apiRoutes);
+
+app.listen(port, function () {
+    console.log("Running RestHub on port " + port);
 });
 
 secIO.on('connection', function(socket) {
@@ -46,26 +58,4 @@ secIO.on('connection', function(socket) {
 
 sessionController.expireSessions(secIO);
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "'GET,PUT,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
-
 //mongoose.connect("mongodb://localhost/TestingDatas", { useNewUrlParser: true });
-
-var port = process.env.PORT || 8080;
-
-app.use('/', apiRoutes);
-
-app.use(express.static('web'));
-
-app.listen(port, function () {
-    console.log("Running RestHub on port " + port);
-});
